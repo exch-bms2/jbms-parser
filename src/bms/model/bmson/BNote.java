2aab7bdf3956e9ba6fe55c038597e03e4bfa32f3
exch-bms2 <exch-bms2@users.noreply.github.com>
2018-07-29 01:05:48 +0900

    fix previous commit

--- src/bms/model/bmson/BNote.java
+++ src/bms/model/bmson/BNote.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 public class BNote extends BMSONObject {
 	public int id; // as it is.
+	public int[] id_set; // as it is.
 	public String condition;
 	public int interval;
 }
