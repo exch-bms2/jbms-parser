bd37a7aba8e7d10183abcd655bc9a3461fc308b2
exch-bms2 <exch-bms2@users.noreply.github.com>
2019-02-09 01:31:57 +0900

    Section : refactor

--- src/bms/model/Section.java
+++ src/bms/model/Section.java
@@ -15,6 +15,7 @@ import static bms.model.DecodeLog.State.*;
  */
 public class Section {
 
+	public static final int ILLEGAL = -1;
 	public static final int LANE_AUTOPLAY = 1;
 	public static final int SECTION_RATE = 2;
 	public static final int BPM_CHANGE = 3;
@@ -39,53 +40,9 @@ public class Section {
 	 */
 	private double rate = 1.0;
 	/**
-	 * BGレーン
-	 */
-	private final int[][] auto;
-	/**
-	 * BGA
-	 */
-	private int[] bga;
-	/**
-	 * レイヤー
-	 */
-	private int[] layer;
-	/**
 	 * POORアニメーション
 	 */
 	private int[] poor = new int[0];
-	/**
-	 * 1P通常ノート
-	 */
-	private int[][] play_1 = new int[9][];
-	/**
-	 * 1P不可視ノート
-	 */
-	private int[][] play_1_invisible = new int[9][];
-	/**
-	 * 1Pロングノート
-	 */
-	private int[][] play_1_ln = new int[9][];
-	/**
-	 * 1P地雷ノート
-	 */
-	private int[][] play_1_mine = new int[9][];
-	/**
-	 * 2P通常ノート
-	 */
-	private int[][] play_2 = new int[9][];
-	/**
-	 * 2P不可視ノート
-	 */
-	private int[][] play_2_invisible = new int[9][];
-	/**
-	 * 2Pロングノート
-	 */
-	private int[][] play_2_ln = new int[9][];
-	/**
-	 * 2P地雷ノート
-	 */
-	private int[][] play_2_mine = new int[9][];
 
 	private Mode mode = Mode.BEAT_5K;
 
@@ -94,12 +51,15 @@ public class Section {
 	private final double sectionnum;
 
 	private final List<DecodeLog> log;
+	
+	private List<String> channellines;
 
 	public Section(BMSModel model, Section prev, List<String> lines, Map<Integer, Double> bpmtable,
 			Map<Integer, Double> stoptable, Map<Integer, Double> scrolltable, List<DecodeLog> log) {
 		this.model = model;
 		this.log = log;
-		final  List<int[]> auto = new ArrayList<int[]>();
+		
+		channellines = new ArrayList<String>(lines.size());
 		mode = model.getMode() == Mode.POPN_9K ? Mode.POPN_9K : Mode.BEAT_5K;
 		if (prev != null) {
 			sectionnum = prev.sectionnum + prev.rate;
@@ -107,38 +67,18 @@ public class Section {
 			sectionnum = 0;
 		}
 		for (String line : lines) {
-			int channel = 0;
-			try {
-				final char c1 = line.charAt(4);
-				final char c2 = line.charAt(5);
-				
-				if((c1 == 'S' || c1 == 's') && (c2 == 'C' || c2 == 's')) {
-					// scroll
-					channel = SCROLL;
-				} else {
-					if (c1 >= '0' && c1 <= '9') {
-						channel = (c1 - '0') * 10;
-					} else if (c1 >= 'a' && c1 <= 'z') {
-						channel = ((c1 - 'a') + 10) * 10;
-					} else if (c1 >= 'A' && c1 <= 'Z') {
-						channel = ((c1 - 'A') + 10) * 10;
-					} else {
-						throw new NumberFormatException();
-					}
-
-					if (c2 >= '0' && c2 <= '9') {
-						channel += (c2 - '0');
-					} else {
-						throw new NumberFormatException();
-					}					
-				}
-			} catch (NumberFormatException e) {
-				log.add(new DecodeLog(WARNING, "チャンネル定義が無効です : " + line));
-			}
+			int channel = getChannel(line);
 			switch (channel) {
+			case ILLEGAL:
+				log.add(new DecodeLog(WARNING, "チャンネル定義が無効です : " + line));
+				break;
 			// BGレーン
 			case LANE_AUTOPLAY:
-				auto.add(this.splitData(line));
+				// BGAレーン
+			case BGA_PLAY:
+			// レイヤー
+			case LAYER_PLAY:
+				channellines.add(line);
 				break;
 			// 小節の拡大率
 			case SECTION_RATE:
@@ -151,15 +91,9 @@ public class Section {
 				break;
 			// BPM変化
 			case BPM_CHANGE:
-				this.splitData(line, (pos, data) -> {
-					if (data != 0) {
-						bpmchange.put(pos, (double) (data / 36) * 16 + (data % 36));
-					}					
+				this.processData(line, (pos, data) -> {
+					bpmchange.put(pos, (double) (data / 36) * 16 + (data % 36));
 				});
-				break;
-			// BGAレーン
-			case BGA_PLAY:
-				bga = this.mergeData(bga, this.splitData(line));
 				break;
 			// POORアニメーション
 			case POOR_PLAY:
@@ -180,134 +114,79 @@ public class Section {
 					poor = new int[] {singleid};
 				}
 				break;
-			// レイヤー
-			case LAYER_PLAY:
-				layer = this.mergeData(layer, this.splitData(line));
-				break;
 			// BPM変化(拡張)
 			case BPM_CHANGE_EXTEND:
-				this.splitData(line, (pos, data) -> {
-					if (data != 0) {
-						Double bpm = bpmtable.get(data);
-						if (bpm != null) {
-							bpmchange.put(pos, bpm);
-						} else {
-							log.add(new DecodeLog(WARNING, "未定義のBPM変化を参照しています : " + data));
-						}
-					}					
+				this.processData(line, (pos, data) -> {
+					Double bpm = bpmtable.get(data);
+					if (bpm != null) {
+						bpmchange.put(pos, bpm);
+					} else {
+						log.add(new DecodeLog(WARNING, "未定義のBPM変化を参照しています : " + data));
+					}
 				});
 				break;
 			// ストップシーケンス
 			case STOP:
-				this.splitData(line, (pos, data) -> {
-					if (data != 0) {
-						Double st = stoptable.get(data);
-						if (st != null) {
-							stop.put(pos, st);
-						} else {
-							log.add(new DecodeLog(WARNING, "未定義のSTOPを参照しています : " + data));
-						}
-					}					
+				this.processData(line, (pos, data) -> {
+					Double st = stoptable.get(data);
+					if (st != null) {
+						stop.put(pos, st);
+					} else {
+						log.add(new DecodeLog(WARNING, "未定義のSTOPを参照しています : " + data));
+					}
 				});
 				break;
 				// scroll
 			case SCROLL:
-				this.splitData(line, (pos, data) -> {
-					if (data != 0) {
-						Double st = scrolltable.get(data);
-						if (st != null) {
-							scroll.put(pos, st);
-						} else {
-							log.add(new DecodeLog(WARNING, "未定義のSCROLLを参照しています : " + data));
-						}
-					}					
+				this.processData(line, (pos, data) -> {
+					Double st = scrolltable.get(data);
+					if (st != null) {
+						scroll.put(pos, st);
+					} else {
+						log.add(new DecodeLog(WARNING, "未定義のSCROLLを参照しています : " + data));
+					}
 				});
 				break;
 			}
 			// 通常ノート(1P側)
-			this.convert(channel, P1_KEY_BASE, play_1, line);
+			this.convert(channel, P1_KEY_BASE, line);
 			// 通常ノート(2P側)
-			this.convert(channel, P2_KEY_BASE, play_2, line);
+			this.convert(channel, P2_KEY_BASE, line);
 			// 不可視ノート(1P側)
-			this.convert(channel, P1_INVISIBLE_KEY_BASE, play_1_invisible, line);
+			this.convert(channel, P1_INVISIBLE_KEY_BASE, line);
 			// 不可視ノート(2P側)
-			this.convert(channel, P2_INVISIBLE_KEY_BASE, play_2_invisible, line);
+			this.convert(channel, P2_INVISIBLE_KEY_BASE, line);
 			// ロングノート(1P側)
-			this.convert(channel, P1_LONG_KEY_BASE, play_1_ln, line);
+			this.convert(channel, P1_LONG_KEY_BASE, line);
 			// ロングノート(2P側)
-			this.convert(channel, P2_LONG_KEY_BASE, play_2_ln, line);
+			this.convert(channel, P2_LONG_KEY_BASE, line);
 			// 地雷ノート(1P側)
-			this.convert(channel, P1_MINE_KEY_BASE, play_1_mine, line);
+			this.convert(channel, P1_MINE_KEY_BASE, line);
 			// 地雷ノート(2P側)
-			this.convert(channel, P2_MINE_KEY_BASE, play_2_mine, line);
+			this.convert(channel, P2_MINE_KEY_BASE, line);
 		}
-		if (auto.size() == 0) {
-			auto.add(new int[] { 0 });
-		}
-		this.auto = auto.toArray(new int[auto.size()][]);
 		if (model.getMode() == null || model.getMode().key < mode.key) {
 			model.setMode(mode);
 		}
 	}
 	
-	private void convert(int channel, int ch, int[][] notes, String line) {
+	private void convert(int channel, int ch, String line) {
 		if (ch <= channel && channel <= ch + 8) {
-			channel -= ch;
-			if (channel == 5 || channel == 6) {
-				channel += 2;
-			} else if (channel == 7 || channel == 8) {
-				if (mode == Mode.BEAT_5K) {
-					mode = Mode.BEAT_7K;
-				} else if(mode == Mode.BEAT_10K){
-					mode = Mode.BEAT_14K;
-				}
-				channel -= 2;
+			channel -= ch;			
+			if((mode == Mode.BEAT_5K || mode == Mode.BEAT_10K) && (channel == 7 || channel == 8)) {
+				this.processData(line, (pos, data) -> {
+					mode = (mode == Mode.BEAT_10K) ? Mode.BEAT_14K : Mode.BEAT_7K;
+				});
 			}
 			
-			int[] split = this.splitData(line);
 			if((mode == Mode.BEAT_5K || mode == Mode.BEAT_7K) && (ch == P2_KEY_BASE || ch == P2_INVISIBLE_KEY_BASE || ch == P2_LONG_KEY_BASE || ch ==P2_MINE_KEY_BASE)) {
-				for(int i : split) {
-					if(i != 0) {
-						if (mode == Mode.BEAT_5K) {
-							mode = Mode.BEAT_10K;
-						} else if(mode == Mode.BEAT_7K){
-							mode = Mode.BEAT_14K;
-						}
-						break;
-					}
-				}
+				this.processData(line, (pos, data) -> {
+					mode = (mode == Mode.BEAT_7K) ? Mode.BEAT_14K : Mode.BEAT_10K;
+				});
 			}
-			notes[channel] = this.mergeData(notes[channel], split);
+			
+			channellines.add(line);
 		}
-	}
-
-	/**
-	 * ノーツのマージ処理を行う
-	 * 
-	 * @param b
-	 *            マージするノーツ
-	 */
-	private int[] mergeData(int[] a, int[] b) {
-		if (a == null || a.length == 0) {
-			return b;
-		}
-		if (b == null || b.length == 0) {
-			return a;
-		}
-		int d = (a.length % b.length == 0 ? b.length : (b.length % a.length == 0 ? a.length : 1));
-		int[] result = new int[a.length * b.length / d];
-		Arrays.fill(result, 0);
-		for (int i = 0; i < a.length; i++) {
-			if (a[i] != 0) {
-				result[i * b.length / d] = a[i];
-			}
-		}
-		for (int i = 0; i < b.length; i++) {
-			if (b[i] != 0) {
-				result[i * a.length / d] = b[i];
-			}
-		}
-		return result;
 	}
 
 	private int[] splitData(String line) {
@@ -324,16 +203,44 @@ public class Section {
 		}
 		return result;			
 	}
+	
+	private int getChannel(String line) {
+		final char c1 = line.charAt(4);
+		final char c2 = line.charAt(5);
+		
+		if((c1 == 'S' || c1 == 's') && (c2 == 'C' || c2 == 's')) {
+			// scroll
+			return SCROLL;
+		} else {
+			int channel = 0;
+			if (c1 >= '0' && c1 <= '9') {
+				channel = (c1 - '0') * 10;
+			} else if (c1 >= 'a' && c1 <= 'z') {
+				channel = ((c1 - 'a') + 10) * 10;
+			} else if (c1 >= 'A' && c1 <= 'Z') {
+				channel = ((c1 - 'A') + 10) * 10;
+			} else {
+				return ILLEGAL;
+			}
 
-	private void splitData(String line, DataProcessor processor) {
+			if (c2 >= '0' && c2 <= '9') {
+				channel += (c2 - '0');
+			} else {
+				return ILLEGAL;
+			}					
+			return channel;
+		}
+	}
+
+	private void processData(String line, DataProcessor processor) {
 		final int findex = line.indexOf(":") + 1;
 		final int lindex = line.length();
 		final int split = (lindex - findex) / 2;
 		for (int i = 0; i < split; i++) {
 			int result = BMSDecoder.parseInt36(line.charAt(findex + i * 2), line.charAt(findex + i * 2 + 1));
-			if(result != -1) {
+			if(result > 0) {
 				processor.process((double)i / split, result);
-			} else {
+			} else if(result == -1){
 				log.add(new DecodeLog(WARNING, model.getTitle() + ":チャンネル定義中の不正な値:" + line));				
 			}
 		}
@@ -347,9 +254,9 @@ public class Section {
 	private final TreeMap<Double, Double> stop = new TreeMap<Double, Double>();
 	private final TreeMap<Double, Double> scroll = new TreeMap<Double, Double>();
 	
-	private static final int[] NOTEASSIGN_BEAT5 = { 0, 1, 2, 3, 4, -1, -1, 5, -1, 6, 7, 8, 9, 10, -1, -1, 11, -1 };
-	private static final int[] NOTEASSIGN_BEAT7 = { 0, 1, 2, 3, 4, 5, 6, 7, -1, 8, 9, 10, 11, 12, 13, 14, 15, -1 };
-	private static final int[] NOTEASSIGN_POPN = { 0, 1, 2, 3, 4, -1,-1,-1,-1,-1, 5, 6, 7, 8,-1,-1,-1,-1 };
+	private static final int[] CHANNELASSIGN_BEAT5 = { 0, 1, 2, 3, 4, 5, -1, -1, -1, 6, 7, 8, 9, 10, 11, -1, -1, -1 };
+	private static final int[] CHANNELASSIGN_BEAT7 = { 0, 1, 2, 3, 4, 7, -1, 5, 6, 8, 9, 10, 11, 12, 15, -1, 13, 14 };
+	private static final int[] CHANNELASSIGN_POPN = { 0, 1, 2, 3, 4, -1,-1,-1,-1,-1, 5, 6, 7, 8,-1,-1,-1,-1 };
 
 	private TreeMap<Double, TimeLineCache> tlcache;
 
@@ -360,8 +267,8 @@ public class Section {
 		final int lnobj = model.getLnobj();
 		final int lnmode = model.getLnmode();
 		this.tlcache = tlcache;
-		final int[] assign = model.getMode() == Mode.POPN_9K ? NOTEASSIGN_POPN : 
-			(model.getMode() == Mode.BEAT_7K || model.getMode() == Mode.BEAT_14K ? NOTEASSIGN_BEAT7 : NOTEASSIGN_BEAT5);
+		final int[] cassign = model.getMode() == Mode.POPN_9K ? CHANNELASSIGN_POPN : 
+			(model.getMode() == Mode.BEAT_7K || model.getMode() == Mode.BEAT_14K ? CHANNELASSIGN_BEAT7 : CHANNELASSIGN_BEAT5);
 		// 小節線追加
 		final TimeLine basetl = getTimeLine(sectionnum);
 		basetl.setSectionLine(true);
@@ -405,54 +312,48 @@ public class Section {
 			}
 		}
 		
-		// 通常ノート配置
-		final int size = 74 + auto.length;
-		for (int keys = 0; keys < size; keys++) {
-			int[] s = null;
-			if (keys >= 0 && keys < 9) {
-				s = this.play_1[keys];
-			} else if (keys >= 9 && keys < 18) {
-				s = this.play_2[keys - 9];
-			} else if (keys >= 18 && keys < 27) {
-				s = this.play_1_invisible[keys - 18];
-			} else if (keys >= 27 && keys < 36) {
-				s = this.play_2_invisible[keys - 27];
-			} else if (keys >= 36 && keys < 45) {
-				s = this.play_1_ln[keys - 36];
-			} else if (keys >= 45 && keys < 54) {
-				s = this.play_2_ln[keys - 45];
-			} else if (keys >= 54 && keys < 63) {
-				s = this.play_1_mine[keys - 54];
-			} else if (keys >= 63 && keys < 72) {
-				s = this.play_2_mine[keys - 63];
-			} else if (keys == 72) {
-				s = this.bga;
-			} else if (keys == 73) {
-				s = this.layer;
-			} else if (keys >= 74) {
-				s = this.auto[keys - 74];
+		for(String line : channellines) {
+			int channel = getChannel(line);
+			int tmpkey = 0;
+			if(channel >= P1_KEY_BASE && channel < P1_KEY_BASE + 9) {
+				tmpkey = cassign[channel - P1_KEY_BASE];
+				channel = P1_KEY_BASE;
+			} else if(channel >= P2_KEY_BASE && channel < P2_KEY_BASE + 9) {
+				tmpkey = cassign[channel - P2_KEY_BASE + 9];
+				channel = P1_KEY_BASE;
+			} else if(channel >= P1_INVISIBLE_KEY_BASE && channel < P1_INVISIBLE_KEY_BASE + 9) {
+				tmpkey = cassign[channel - P1_INVISIBLE_KEY_BASE];
+				channel = P1_INVISIBLE_KEY_BASE;
+			} else if(channel >= P2_INVISIBLE_KEY_BASE && channel < P2_INVISIBLE_KEY_BASE + 9) {
+				tmpkey = cassign[channel - P2_INVISIBLE_KEY_BASE + 9];
+				channel = P1_INVISIBLE_KEY_BASE;
+			} else if(channel >= P1_LONG_KEY_BASE && channel < P1_LONG_KEY_BASE + 9) {
+				tmpkey = cassign[channel - P1_LONG_KEY_BASE];
+				channel = P1_LONG_KEY_BASE;
+			} else if(channel >= P2_LONG_KEY_BASE && channel < P2_LONG_KEY_BASE + 9) {
+				tmpkey = cassign[channel - P2_LONG_KEY_BASE + 9];
+				channel = P1_LONG_KEY_BASE;
+			} else if(channel >= P1_MINE_KEY_BASE && channel < P1_MINE_KEY_BASE + 9) {
+				tmpkey = cassign[channel - P1_MINE_KEY_BASE];
+				channel = P1_MINE_KEY_BASE;
+			} else if(channel >= P2_MINE_KEY_BASE && channel < P2_MINE_KEY_BASE + 9) {
+				tmpkey = cassign[channel - P2_MINE_KEY_BASE + 9];
+				channel = P1_MINE_KEY_BASE;
 			}
-			if(s == null) {
-				continue;
-			}
-			final int slength = s.length;
-			final int key = keys < 72 ? assign[keys % 18] : 0;
+			final int key = tmpkey;
 			if(key == -1) {
 				continue;
 			}
-			
-			for (int i = 0; i < slength; i++) {
-				if (s[i] == 0) {
-					continue;
-				}
-				final TimeLine tl = getTimeLine(sectionnum + rate * i / slength);
-				if (keys < 18) {
+			switch (channel) {
+			case P1_KEY_BASE:
+				this.processData(line, (pos, data) -> {
 					// normal note, lnobj
+					final TimeLine tl = getTimeLine(sectionnum + rate * pos);
 					if (tl.existNote(key)) {
 						log.add(new DecodeLog(WARNING, "通常ノート追加時に衝突が発生しました : " + (key + 1) + ":"
 								+ tl.getTime()));
 					}
-					if (s[i] == lnobj) {
+					if (data == lnobj) {
 						// LN終端処理
 						// TODO 高速化のために直前のノートを記録しておく
 						for (Map.Entry<Double, TimeLineCache> e : tlcache.descendingMap().entrySet()) {
@@ -490,14 +391,19 @@ public class Section {
 							}
 						}
 					} else {
-						tl.setNote(key, new NormalNote(wavmap[s[i]]));
+						tl.setNote(key, new NormalNote(wavmap[data]));
 					}							
-				} else if (keys >= 18 && keys < 36) {
-					// hidden note
-					tl.setHiddenNote(key, new NormalNote(wavmap[s[i]]));							
-					// Logger.getGlobal().warning(model.getTitle() + "隠しノート追加"+ (key - 17) + ":" + (base + (int) (dt * rate)));
-				} else if (keys >= 36 && keys < 54) {
+				});
+				break;				
+			case P1_INVISIBLE_KEY_BASE:
+				this.processData(line, (pos, data) -> {
+					getTimeLine(sectionnum + rate * pos).setHiddenNote(key, new NormalNote(wavmap[data]));
+				});
+				break;				
+			case P1_LONG_KEY_BASE:
+				this.processData(line, (pos, data) -> {
 					// long note
+					final TimeLine tl = getTimeLine(sectionnum + rate * pos);
 					boolean insideln = tl.existNote(key);
 					if (!insideln && lnlist[key] != null) {
 						final double section = tl.getSection();
@@ -512,7 +418,7 @@ public class Section {
 					if(!insideln) {
 						// LN処理
 						if (startln[key] == null) {
-							LongNote ln = new LongNote(wavmap[s[i]]);
+							LongNote ln = new LongNote(wavmap[data]);
 							tl.setNote(key, ln);
 							startln[key] = ln;
 						} else if(startln[key].getSection() == Double.MIN_VALUE){
@@ -528,7 +434,7 @@ public class Section {
 								if(tl2.getSection() == startln[key].getSection()) {
 									Note note = startln[key];
 									((LongNote)note).setType(lnmode);
-									LongNote noteend = new LongNote(startln[key].getWav() != wavmap[s[i]] ? wavmap[s[i]] : -2);
+									LongNote noteend = new LongNote(startln[key].getWav() != wavmap[data] ? wavmap[data] : -2);
 									tl.setNote(key, noteend);
 									((LongNote)note).setPair(noteend);
 									if (lnlist[key] == null) {
@@ -551,7 +457,7 @@ public class Section {
 						}								
 					} else {
 						if (startln[key] == null) {
-							LongNote ln = new LongNote(wavmap[s[i]]);
+							LongNote ln = new LongNote(wavmap[data]);
 							ln.setSection(Double.MIN_VALUE);
 							startln[key] = ln;
 							log.add(new DecodeLog(WARNING, "LN内にLN開始ノートを定義しようとしています : "
@@ -565,8 +471,12 @@ public class Section {
 									+ (key + 1) + " - Section : " + tl.getSection() + " - Time(ms):" + tl.getTime()));
 						}
 					}
-				} else if (keys >= 54 && keys < 72) {
-					// mine note
+				});
+				break;				
+			case P1_MINE_KEY_BASE:
+				// mine note
+				this.processData(line, (pos, data) -> {
+					final TimeLine tl = getTimeLine(sectionnum + rate * pos);
 					boolean insideln = tl.existNote(key);
 					if (!insideln && lnlist[key] != null) {
 						final double section = tl.getSection();
@@ -579,18 +489,30 @@ public class Section {
 					}
 
 					if(!insideln) {
-						tl.setNote(key, new MineNote(wavmap[0], s[i]));								
+						tl.setNote(key, new MineNote(wavmap[0], data));								
 					} else {
 						log.add(new DecodeLog(WARNING, "地雷ノート追加時に衝突が発生しました : " + (key + 1) + ":"
 								+ tl.getTime()));								
 					}
-				} else if (keys == 72) {
-					tl.setBGA(bgamap[s[i]]);
-				} else if (keys == 73) {
-					tl.setLayer(bgamap[s[i]]);
-				} else if (keys >= 74) {
-					tl.addBackGroundNote(new NormalNote(wavmap[s[i]]));
-				}
+				});
+				break;
+			case LANE_AUTOPLAY:
+				// BGレーン
+				this.processData(line, (pos, data) -> {
+					getTimeLine(sectionnum + rate * pos).addBackGroundNote(new NormalNote(wavmap[data]));
+				});
+				break;
+			case BGA_PLAY:
+				this.processData(line, (pos, data) -> {
+					getTimeLine(sectionnum + rate * pos).setBGA(bgamap[data]);
+				});
+				break;
+			case LAYER_PLAY:
+				this.processData(line, (pos, data) -> {
+					getTimeLine(sectionnum + rate * pos).setLayer(bgamap[data]);
+				});
+				break;
+
 			}
 		}
 	}
